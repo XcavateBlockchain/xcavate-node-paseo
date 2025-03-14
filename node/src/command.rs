fd2b71e73939bec534b794721daf50fd66d3181b
Recrafter <renehurter0@gmail.com>
2025-03-14 09:36:05 +0100

    Adjusting command.rs

--- node/src/command.rs
+++ node/src/command.rs
@@ -30,7 +30,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 
 impl SubstrateCli for Cli {
     fn impl_name() -> String {
-        "Parachain Collator Template".into()
+        "Xcavate Collator".into()
     }
 
     fn impl_version() -> String {
@@ -39,7 +39,7 @@ impl SubstrateCli for Cli {
 
     fn description() -> String {
         format!(
-            "Parachain Collator Template\n\nThe command-line arguments provided first will be \
+            "Xcavate Collator\n\nThe command-line arguments provided first will be \
              passed to the parachain node, while the arguments provided after -- will be passed \
              to the relay chain node.\n\n{} <parachain-args> -- <relay-chain-args>",
             Self::executable_name()
@@ -51,7 +51,7 @@ impl SubstrateCli for Cli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/polkadot-sdk/issues/new".into()
+        "https://github.com/XcavateBlockchain/xcavate-node-paseo/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
@@ -65,7 +65,7 @@ impl SubstrateCli for Cli {
 
 impl SubstrateCli for RelayChainCli {
     fn impl_name() -> String {
-        "Parachain Collator Template".into()
+        "Xcavate Collator".into()
     }
 
     fn impl_version() -> String {
@@ -74,7 +74,7 @@ impl SubstrateCli for RelayChainCli {
 
     fn description() -> String {
         format!(
-            "Parachain Collator Template\n\nThe command-line arguments provided first will be \
+            "Xcavate Collator\n\nThe command-line arguments provided first will be \
              passed to the parachain node, while the arguments provided after -- will be passed \
              to the relay chain node.\n\n{} <parachain-args> -- <relay-chain-args>",
             Self::executable_name()
@@ -86,7 +86,7 @@ impl SubstrateCli for RelayChainCli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/polkadot-sdk/issues/new".into()
+        "https://github.com/XcavateBlockchain/xcavate-node-paseo/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
