e2df50eaf401d3084ede884ffa04c8d228294a98
Ganesh <ganesh@Ganesh-Mac.local>
2025-05-29 20:36:54 +0545

    update typo

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -30,11 +30,11 @@ pub const POLY_DEGREE: u8 = 1;
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("template-parachain"),
     impl_name: create_runtime_str!("template-parachain"),
-    authoring_version: 2,
+    authoring_version: 1,
     spec_version: 2,
     impl_version: 0,
     apis: apis::RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
     state_version: 1,
 };
 
